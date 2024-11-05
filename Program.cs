fd5cd6650de9c9d3c2190f0526a0907dfdca9b32
stephenjtyrrell <stephenjtyrrell@users.noreply.github.com>
2024-11-05 20:05:55 +0000

    adding console app

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ProgramClass {
+
+    public class Person
+{
+  private string name; // field
+  public string Name   // property
+  {
+    get { return name; }
+    set { name = value; }
+  }
+}
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+
+    Person myObj = new Person();
+    foreach(string name in args){
+        myObj.Name = name;
+    }
+    Console.WriteLine(myObj.Name);
+  }
+}
+
+}
+
